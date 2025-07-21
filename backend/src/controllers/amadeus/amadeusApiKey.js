d7b49190ae2f393ede057cd3429c3322d48543ca
Gustavo Bolivar <gusta_bolivar@hotmail.com>
2025-07-22 00:34:24 +0200

    Add logs for Amadeus API key and secret initialization

--- backend/src/controllers/amadeus/amadeusApiKey.js
+++ backend/src/controllers/amadeus/amadeusApiKey.js
@@ -1,9 +1,18 @@
 import "dotenv/config";
 import Amadeus from 'amadeus';
 
+
+
+console.log('✅ Cargando Amadeus...');
+console.log('🔑 API Key:', process.env.AMADEUS_API_KEY);
+console.log('🔒 API Secret:', process.env.AMADEUS_API_SECRET);
+console.log('🌍 Entorno:', process.env.NODE_ENV);
+
+
 const amadeus = new Amadeus({
     clientId: process.env.AMADEUS_API_KEY,
     clientSecret: process.env.AMADEUS_API_SECRET,
+    hostname: process.env.NODE_ENV === 'production' ? 'api.amadeus.com' : 'test.api.amadeus.com',// Cambia el hostname según el entorno
 });
 
 export default amadeus;
